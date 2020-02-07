604970894a3e33de3d367fd755e7ad54c3e01e4f
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-06 20:28:04 -0500

    Feat - React hook event from JSX

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,10 +4,22 @@ const Product = ({product}) => {
     
     const { name, price, id } = product;
 
+    // add product to cart
+    const selectProduct = (id) => {
+        console.log('Product added', id);
+    }
+
     return ( 
         <div id={id}>
             <h3>{name}</h3>
-            <p>{price}</p>
+            <p>$ {price}</p>
+            <button 
+                type="button"
+                id={`button-${id}`}
+                onClick={ () => selectProduct(id)}
+            >
+                Comprar
+            </button>
         </div> 
     );
 
