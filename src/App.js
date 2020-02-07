3c8f828a0c09395e4d766d5e2a6eecbb1101e41e
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-06 19:54:00 -0500

    Feat - create stateles function component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Header/>
+    <Fragment>
+      <Header/>
+      <Footer/>
+    </Fragment>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => ( 
+        <footer>
+            <p>All rights reserved &copy;</p>
+        </footer>
+);
+ 
+
+export default Footer;
